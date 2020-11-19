ab51018a3a42015216f86ec9d2b13d56e0f0e75d
Patrik Mada <mada.patrik@gmail.com>
2020-11-19 15:50:47 +0100

    docs: Add javadoc style commentary.

--- src/main.c
+++ src/main.c
@@ -2,39 +2,54 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Allocation function
+/** Allocation function */
 float* allocate(int size) {
     float* arr = malloc(size * sizeof(float));
     return arr;
 }
 
-// Copy input vector into dynamically alocated field
+/** Copy input vector into dynamically alocated field */
 int copyInputToField(float* arr, int size) {
+
     for (int i = 0; i < size; i++) {
         scanf("%f", &arr[i]);
     }
+
     return 0;
 }
-  
-float sum_exponentials(float* arr, const int len) {  
-    float sum = 0; 
-    for (int i = 0; i < len; i++)  {
-         sum += exp(arr[i]);
-    }  
-    return sum;  
-} 
 
+/** Softmax function transforms input vector into a vector of probability distribution.
+ * 
+ * Generalization of logistic function to multiple dimensions.
+ * For given input vector of given length `len`, calculates probability distribution
+ * of `len` possible outcomes.
+ * 
+ * @param arr   A pointer to an input vector (array of floats).
+ * @param len   A length of input vector
+ */
 float* softmax(float* arr, const int len) {
-    float softmax_denominator = sum_exponentials(arr, len);
+    float softmax_denominator = 0;
+
+    for (int i = 0; i < len; i++)  {
+         softmax_denominator += exp(arr[i]);
+    }  
 
     for (int i = 0; i < len; i++) {
         float computed_value = exp(arr[i]) / softmax_denominator;
         arr[i] = computed_value;
     }
 
-    return arr;
+    return 0;
 }
 
+/** Argmax function returns and index of an element with the largest likelihood,
+ * according to probability distribution vector calculated by softmax.
+ * 
+ * @param arr   A pointer to a probability distribution vector (array of floats).
+ * @param len   A length of a probability distribution vector.
+ * 
+ * @returns Index of the element with the largest likelihood.
+ */
 int argmax(float* arr, const int len) {
     int index_max = 0;
     float max = arr[0];
@@ -49,6 +64,7 @@ int argmax(float* arr, const int len) {
     return index_max;
 }
 
+/** Prints output into STDOUT in defined format */
 int print_result(int current_argmax, float* current_softmax, const int len) {
     printf("%d", current_argmax);
 
@@ -62,6 +78,7 @@ int print_result(int current_argmax, float* current_softmax, const int len) {
 }
 
 int main() {
+
     // Input vector size is given
     const int INPUT_VECTOR_SIZE = 10;
 
@@ -73,13 +90,12 @@ int main() {
     for (int i = 0; i < input_vectors_count; i++) {
         copyInputToField(arr_pointer, INPUT_VECTOR_SIZE);
 
-        float* current_softmax = softmax(arr_pointer, INPUT_VECTOR_SIZE);
+        softmax(arr_pointer, INPUT_VECTOR_SIZE);
         int current_argmax = argmax(arr_pointer, INPUT_VECTOR_SIZE);
 
-        print_result(current_argmax, current_softmax, INPUT_VECTOR_SIZE);
+        print_result(current_argmax, arr_pointer, INPUT_VECTOR_SIZE);
     }
 
     free(arr_pointer);
-
     return 0;
 }
